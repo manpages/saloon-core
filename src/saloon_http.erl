63085830d696f675913f47665390f635dd2a1a88
Jonns Mostovoys <amarr.industrial@gmail.com>
2012-09-04 22:10:05 +0300

    added debug output

--- src/saloon_http.erl
+++ src/saloon_http.erl
@@ -8,6 +8,7 @@
 
 -include_lib("cowboy/include/http.hrl").
 -include_lib("saloon/include/file.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 %%
 %% Simple method suitable for simplistic return of HTML.
@@ -31,14 +32,18 @@ return(Reply, Req) ->
 receive_file(Req@, State) ->
 	case cowboy_http_req:multipart_data(Req@) of
 		{error, badarg} ->
+			?debugMsg("No multipart data, 500"),
 			{ok, Req@} = cowboy_http_req:reply(500, Req@),
 			{Req@, State};
 		{{headers, Headers}, _Req@} ->
+			?debugMsg("Parsing headers"),
 			Disp = cowboy_multipart:content_disposition(proplists:get_value(<<"Content-Disposition">>, Headers)),
 			case kvc:path('form-data'.filename, [Disp]) of
 				[] -> %% not a file
+					?debugMsg("Not a file"),
 					receive_file(Req@, State);
 				FilenameB ->
+					?debugFmt("File ~p", [FilenameB]),
 					ContentType = proplists:get_value('Content-Type', Headers,
 													  <<"application/octet-stream">>),
 					Filename = binary_to_list(FilenameB),
@@ -51,18 +56,23 @@ receive_file(Req@, State) ->
 							  })
 			end;
 		{{body, Data}, Req@} ->
+			?debugMsg("Got body"),
 			if 
 				State#upload_state.uploader /= undefined ->
+					?debugFmt("Uploading through uploader at ~p", [State#upload_state.uploader]),
 					saloon_uploader:write(State#upload_state.uploader, Data),
 					receive_file(Req@, State#upload_state{ size = size(Data) + State#upload_state.size });
 				true ->
+					?debugMsg("Wtf?"),
 					receive_file(Req@, State)
 			end;
 		{end_of_part, Req@} ->
+			?debugMsg("Got end of part"),
 			if
 				State#upload_state.uploader /= undefined ->
 					case saloon_uploader:done(State#upload_state.uploader) of
 						{ok, URL, Location} ->
+							?debugFmt("Finalizing upload at ~p", [State#upload_state.uploader]),
 							Files = [[
 									  {name, State#upload_state.filename},
 									  {url, URL}]|State#upload_state.files],
@@ -77,9 +87,11 @@ receive_file(Req@, State) ->
 							{Req@, State}
 					end;
 				true ->
+					?debugMsg("Wtf? end_of_part"),
 					receive_file(Req@, State)
 			end;
 		{eof, Req@} ->
+			?debugMsg("Got EOF"),
 			{ok, Req@} = cowboy_http_req:reply(200, 
 											   [{<<"Content-Type">>, <<"application/json">>}],
 											   jsx:to_json(State#upload_state.files),
