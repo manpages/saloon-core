b1822c4b60f51c3d559664ec637dd5809302d372
Jonns Mostovoys <amarr.industrial@gmail.com>
2012-09-04 07:29:31 +0300

    including file upload in the default saloon package :)

---
+++ include/file.hrl
@@ -0,0 +1,7 @@
+-record(state, {
+		 filename
+		,content_type
+		,uploader :: pid()
+		,files = []
+		,size = 0
+	}).
--- src/saloon_http.erl
+++ src/saloon_http.erl
@@ -6,6 +6,7 @@
 
 
 -include_lib("cowboy/include/http.hrl").
+-include_lib("saloon/include/file.hrl").
 
 %%
 %% Simple method suitable for simplistic return of HTML.
@@ -22,3 +23,67 @@ return(Reply, Req) ->
                 ),
 			{ok, RenderedRepReq, return}
 		end.
+
+%%
+%% Handling file upload. Thanks, yrashk, for sharing that snippet!
+%%
+receive_file(Req@, State) ->
+	case cowboy_http_req:multipart_data(Req@) of
+		{error, badarg} ->
+			{ok, Req@} = cowboy_http_req:reply(500, Req@),
+			{Req@, State};
+		{{headers, Headers}, Req@} ->
+			Disp = cowboy_multipart:content_disposition(proplists:get_value(<<"Content-Disposition">>, Headers)),
+			case kvc:path('form-data'.filename, [Disp]) of
+				[] -> %% not a file
+					recv(Req@, State);
+				FilenameB ->
+					ContentType = proplists:get_value('Content-Type', Headers,
+													  <<"application/octet-stream">>),
+					Filename = binary_to_list(FilenameB),
+					{ok, Uploader} = file_uploader:start(FilenameB,
+																ContentType),
+					recv(Req@, 
+							 State#state{ 
+							   filename = Filename,
+							   content_type = ContentType,
+							   uploader = Uploader
+							  })
+			end;
+		{{body, Data}, Req@} ->
+			if 
+				State#state.uploader /= undefined ->
+					file_uploader:write(State#state.uploader, Data),
+					recv(Req@, State#state{ size = size(Data) + State#state.size });
+				true ->
+					recv(Req@, State)
+			end;
+		{end_of_part, Req@} ->
+			if
+				State#state.uploader /= undefined ->
+					case file_uploader:done(State#state.uploader) of
+						{ok, URL, Location} ->
+							Files = [[
+									  {name, FilenameB},
+									  {url, URL}]|State#state.files],
+							recv(Req@, State#state{ filename = undefined,
+													content_type = undefined,
+													uploader = undefined,
+													size = 0,
+													files = Files
+												  });
+						_Error ->
+							{ok, Req@} = cowboy_http_req:reply(500, Req@),
+							{Req@, State}
+					end;
+				true ->
+					recv(Req@, State)
+			end;
+		{eof, Req@} ->
+			{ok, Req@} = cowboy_http_req:reply(200, 
+											   [{<<"Content-Type">>, <<"application/json">>}],
+											   jsx:to_json(State#state.files),
+											   Req@),
+			{Req@, State#state{ files = [] }}
+	end.
+
